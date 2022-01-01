96fb1cd70fa8e3acb96d4dc0136d05f6717608c5
ohwoo <kwonow1@gmail.com>
2022-01-01 13:57:26 +0900

    3.5 Offers

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -12,7 +12,8 @@ call.hidden = true;
 let myStream;
 let muted = false;
 let cameraOff = false;
-let roomName = "";
+let roomName;
+let myPeerConnection;
 
 async function getCameras() {
   try {
@@ -89,10 +90,11 @@ camerasSelect.addEventListener("input", handleCameraChange);
 
 const welcomeForm = welcome.querySelector("form");
 
-function startMedia() {
+async function startMedia() {
   welcome.hidden = true;
   call.hidden = false;
-  getMedia();
+  await getMedia();
+  makeConnection();
 }
 
 function handleWelcomeSubmit(event) {
@@ -107,6 +109,21 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
 // Socket Code
 
-socket.on("welcome", () => {
-  console.log("someone joined");
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  socket.emit("offer", offer, roomName);
 });
+
+socket.on("offer", (offer) => {
+  console.log(offer);
+});
+
+// RTC Code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
